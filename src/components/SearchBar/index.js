9d0a2a8da2d1b72f99b74d5f90906e0022651d33
Leonardo Dominguez <focuux@gmail.com>
2018-04-21 21:20:31 -0400

    Change input and icons

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
 import Grid from 'material-ui/Grid';
-import { Search } from '@material-ui/icons';
+import { LocationOn } from '@material-ui/icons';
 import {
   StyledGrid,
   Container,
   StyledInputGrid,
   StyledBtnGrid,
-  StyledNextIcon,
-  StyledNextBtn
+  StyledSearchIcon,
+  StyledNextBtn,
+  SearchInput,
+  StyledLocIcon
 } from './style';
 
 const SearchBar = () => (
@@ -16,18 +18,13 @@ const SearchBar = () => (
     <Grid container justify="center" alignItems="center">
       <StyledGrid item sm={8}>
         <Grid container justify="center">
-          <StyledInputGrid item sm={10}>
+          <StyledInputGrid item container sm={10} alignItems="flex-end">
             <Grid container justify="center" alignItems="flex-end">
               <Grid item container justify="flex-end" sm={1}>
-                <Search />
+                <StyledLocIcon />
               </Grid>
               <Grid item sm={11}>
-                <TextField
-                  fullWidth
-                  id="search"
-                  type="search"
-                  label="I want to explore.."
-                />
+                <SearchInput type="text" placeholder="Where do you want to go?" />
               </Grid>
             </Grid>
           </StyledInputGrid>
@@ -40,7 +37,7 @@ const SearchBar = () => (
               sm={12}
             >
               <StyledNextBtn variant="raised" size="small" color="primary">
-                <StyledNextIcon />
+                <StyledSearchIcon />
                 SEARCH
               </StyledNextBtn>
             </Grid>
