c716572d8c5f0dd8f85c8098139a94209e9818a4
Leonardo Dominguez <focuux@gmail.com>
2018-05-07 01:00:15 -0400

    Add social button component for fb and google login and sign up

---
+++ src/components/SocialBtn/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Grid } from 'material-ui';
+import { Facebook, Google } from 'mdi-material-ui';
+import { StyledGrid, Button } from './style';
+
+const SocialBtn = ({ type, action, ...others }) => (
+  <Button {...others}>
+    <StyledGrid bgColor={type === 'fb' ? '#3b5999' : '#dd4b39'} container justify="center" alignItems="center">
+      <Grid item container justify="center" alignItems="center" sm={1}>
+        {type === 'fb' ? <Facebook /> : <Google />}
+      </Grid>
+      <Grid item container justify="center" alignItems="center" sm={5}>
+        {action === 'login' ? 'Log in with ' : 'Sign up with '}{type === 'fb' ? 'Facebook' : 'Google'}
+      </Grid>
+    </StyledGrid>
+  </Button>
+);
+
+export default SocialBtn;
---
+++ src/components/SocialBtn/style.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Grid } from 'material-ui';
+
+export const StyledGrid = styled(({ bgColor, ...others }) => <Grid {...others} />)`
+  color: #fff;
+  background-color: ${props => props.bgColor};
+  padding: ${props => props.theme.gap.small};
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  border: 0;
+  transition: opacity .2s ease-out;
+  font-size: 1.8rem;
+  font-weight: 600;
+  letter-spacing: .5px;
+  box-shadow: 0 3px 9px rgba(0,0,0,.3);
+  border-radius: .5rem;
+  overflow: hidden;
+
+  &:hover, &:focus {
+    outline: 0;
+    opacity: .9;
+  }
+`;
