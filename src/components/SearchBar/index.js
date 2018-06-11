cb1fc8e525931dd179c01102612ebcaf96110f47
Leonardo Dominguez <focuux@gmail.com>
2018-06-10 20:43:14 -0400

    Remove unused imports

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import TextField from 'material-ui/TextField';
 import Grid from 'material-ui/Grid';
-import { LocationOn } from '@material-ui/icons';
 import {
   StyledGrid,
   Container,
