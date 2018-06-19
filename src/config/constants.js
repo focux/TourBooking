3c5531f2feba2db7bbc831ad088db36221f22898
Leonardo Dominguez <focuux@gmail.com>
2018-06-18 23:10:55 -0400

    More fixes

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 export default {
   appName: 'Driscovery',
-  apiUrl: window.location.hostname === 'localhost' ? 'http://localhost:3000/api/v1' : 'http://www.driscovery.com'
+  apiUrl: window.location.hostname === 'localhost' ? 'http://localhost:3000/api/v1' : 'http://www.driscovery.com/api/v1'
 };
